e2d53b2a9a6be03c38dbc62d4787810326d0a289
Jakub Waśniewski <wasniewski.jakub@wp.pl>
2023-05-26 12:42:42 +0200

    add python3 by default and pip3 install functionality

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ template = environment.get_template("template-dockerfile.txt")
 OS_image="ubuntu"
 OS_image_version="latest"
 message = "testing message 123"
+items = ["Jinja2", "PyPDF2", "click"]
 content = template.render(OS_image=OS_image,
-                          OS_image_version=OS_image_version)
+                          OS_image_version=OS_image_version,
+                          packages_to_install=items)
 
 
 with open("outputs/Dockerfile", "w") as file:
