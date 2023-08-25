26348eaf2f14342e3c911291d5105aaac26d9980
ola567 <aleksandra.hein@o2.pl>
2023-08-25 22:56:09 +0200

    refactor code - create class

--- searching_imports.py
+++ searching_imports.py
@@ -1,40 +1,38 @@
 import os
-
 from stdlib_list import stdlib_list
 
 
-def search_imports(path_to_project):
-    imports = []
-    for root, dirs, files in os.walk(path_to_project):
-        for file in files:
-            if file.endswith(".py"):
-                with open(os.path.join(root, file), 'r') as f:
-                    for l_no, line in enumerate(f):
-                        if line.strip().startswith('import') or line.strip().startswith('from'):
-                            print(line)
-                            imports.append(line)
-    return imports
+class ModuleSearcher:
+    def __init__(self, path_to_project, python_version='3.11'):
+        self.path_to_project = path_to_project
+        self.python_version = python_version
+        self.modules = []
+
+    def get_modules(self):
+        self._search_imports()
+        self._extract_modules()
+        self._remove_build_in_modules()
+
+    def _search_imports(self):
+        for root, dirs, files in os.walk(self.path_to_project):
+            for file in files:
+                if file.endswith(".py"):
+                    with open(os.path.join(root, file), 'r') as f:
+                        for l_no, line in enumerate(f):
+                            if line.strip().startswith('import') or line.strip().startswith('from'):
+                                self.modules.append(line)
+
+    def _extract_modules(self):
+        mods = [element.split(" ")[1].replace("\n", "") for element in self.modules]
+        mods = list(set(mods))
+        self.modules = mods
+
+    def _remove_build_in_modules(self):
+        libraries = stdlib_list(self.python_version)
+        self.modules = [mod for mod in self.modules if mod not in libraries]
 
 
-def extract_modules(input_array):
-    mods = [element.split(" ")[1].replace("\n", "") for element in input_array]
-    mods = list(set(mods))
-    return mods
-
-
-def remove_build_in_modules(python_version='3.11', input_array=None):
-    if input_array is None:
-        input_array = []
-    libraries = stdlib_list(python_version)
-    mods = [mod for mod in input_array if mod not in libraries]
-    return mods
-
-
-import_lines = search_imports('/home/ola/Desktop/example/database_structures/project2')
-print(import_lines)
-print()
-modules = extract_modules(input_array=import_lines)
-print(modules)
-print()
-print(remove_build_in_modules(input_array=modules))
+module_searcher = ModuleSearcher(path_to_project='/home/ola/Desktop/example/database_structures/project2')
+module_searcher.get_modules()
+print(module_searcher.modules)
 
