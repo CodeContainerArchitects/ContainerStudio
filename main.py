b96029e4535f5fbdbfb0d78fee4951195e8c2956
Aleksandra Hein <76129631+ola567@users.noreply.github.com>
2023-08-25 23:17:53 +0200

    CCA-42-select-files-from-repository-which-will-be-added-to-dockerfile (#3)
    * Add selecting directory and selecting files to copy to Dockerfile.
    
    * Add working custom path for Dockerfile.
    
    * Add copying folders to Dockerfile.
    
    * Change name of the working directory folder.
    
    * Move copy command in dockerfile
    
    ---------
    
    Co-authored-by: IllBeWatchingU <jzegota2001@gmail.com>
    Co-authored-by: IllBeWatchingU <101195052+IllBeWatchingU@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,130 +1,135 @@
 import jinja2
 import subprocess
 import tkinter as tk
+import add_files
+import os
 from requirements_searching import use_requirements
 
-pip_packages = {"jinja": "Jinja2",
+def generate_dockerfile():
+    pip_packages = {"jinja": "Jinja2",
                "mypdf2": "PyPDF2",
                "click": "click"}
 
-apt_get_packages = {"git": "git",
-                    "ansible":"ansible",
-                    "nano": "nano",
-                    "vim": "vim",
-                    "htop": "htop",
-                    "curl": "curl",
-                    "wget": "wget",
-                    "python3": "python3",
-                    "python3-pip": "python3-pip",
-                    "apache2": "apache2",
-                    "nginx": "nginx",
-                    "mysql-server": "mysql-server",
-                    "postgresql": "postgresql",
-                    "php": "php",
-                    "nodejs": "nodejs",
-                    "npm": "npm",
-                    "openjdk-11-jdk": "openjdk-11-jdk",
-                    "gcc": "gcc",
-                    "g++": "g++",
-                    "make": "make",
-                    "virtualbox": "virtualbox",
-                    "docker": "docker",
-                    "docker-compose": "docker-compose",
-                    "ansible": "ansible",
-                    "wireshark": "wireshark",
-                    "openssh-server": "openssh-server",
-                    "tmux": "tmux",
-                    "screen": "screen",
-                    "emacs": "emacs",
-                    "gparted": "gparted",
-                    "ffmpeg": "ffmpeg",
-                    "vlc": "vlc",
-                    "libreoffice": "libreoffice",
-                    "gimp": "gimp",
-                    "inkscape": "inkscape",
-                    "blender": "blender",
-                    "audacity": "audacity",
-                    "rhythmbox": "rhythmbox",
-                    "transmission": "transmission",
-                    "clamav": "clamav",
-                    "fail2ban": "fail2ban",
-                    "ufw": "ufw",
-                    "nmap": "nmap",
-                    "lynx": "lynx",
-                    "elinks": "elinks",
-                    "irssi": "irssi",
-                    "mutt": "mutt",
-                    "transmission-cli": "transmission-cli",
-                    "samba": "samba",
-                    "nfs-common": "nfs-common",
-                    "cifs-utils": "cifs-utils",
-                    "sshfs": "sshfs",
-                    "zip": "zip",
-                    "unzip": "unzip",
-                    "rar": "rar",
-                    "unrar": "unrar",
-                    "gnome-terminal": "gnome-terminal",
-                    "xfce4-terminal": "xfce4-terminal",
-                    "konsole": "konsole",
-                    "terminator": "terminator",
-                    "ranger": "ranger",
-                    "midnight-commander": "midnight-commander",
-                    "synaptic": "synaptic",
-                    "filezilla": "filezilla",
-                    "putty": "putty",
-                    "xrdp": "xrdp",
-                    "remmina": "remmina",
-                    "pidgin": "pidgin",
-                    "hexchat": "hexchat",
-                    "teamviewer": "teamviewer",
-                    "wine": "wine",
-                    "clamtk": "clamtk",
-                    "xclip": "xclip",
-                    "meld": "meld",
-                    "bleachbit": "bleachbit",
-                    "gufw": "gufw",
-                    "gdebi": "gdebi",
-                    "keepassxc": "keepassxc",
-                    "transmission-gtk": "transmission-gtk",
-                    "qbittorrent": "qbittorrent",
-                    "deluge": "deluge",
-                    "hexedit": "hexedit",
-                    "radare2": "radare2",
-                    "netcat": "netcat",
-                    "john": "john",
-                    "hydra": "hydra",
-                    "nikto": "nikto",
-                    "aircrack-ng": "aircrack-ng",
-                    "ettercap-graphical": "ettercap-graphical",
-                    "tcpdump": "tcpdump",
-                    "openssh-client": "openssh-client",
-                    "rsync": "rsync",
-                    "ffmpeg": "ffmpeg",
-                    "audacity": "audacity",
-                    "darktable": "darktable",
-                    "krita": "krita",}
+    apt_get_packages = {"git": "git",
+                        "ansible":"ansible",
+                        "nano": "nano",
+                        "vim": "vim",
+                        "htop": "htop",
+                        "curl": "curl",
+                        "wget": "wget",
+                        "python3": "python3",
+                        "python3-pip": "python3-pip",
+                        "apache2": "apache2",
+                        "nginx": "nginx",
+                        "mysql-server": "mysql-server",
+                        "postgresql": "postgresql",
+                        "php": "php",
+                        "nodejs": "nodejs",
+                        "npm": "npm",
+                        "openjdk-11-jdk": "openjdk-11-jdk",
+                        "gcc": "gcc",
+                        "g++": "g++",
+                        "make": "make",
+                        "virtualbox": "virtualbox",
+                        "docker": "docker",
+                        "docker-compose": "docker-compose",
+                        "ansible": "ansible",
+                        "wireshark": "wireshark",
+                        "openssh-server": "openssh-server",
+                        "tmux": "tmux",
+                        "screen": "screen",
+                        "emacs": "emacs",
+                        "gparted": "gparted",
+                        "ffmpeg": "ffmpeg",
+                        "vlc": "vlc",
+                        "libreoffice": "libreoffice",
+                        "gimp": "gimp",
+                        "inkscape": "inkscape",
+                        "blender": "blender",
+                        "audacity": "audacity",
+                        "rhythmbox": "rhythmbox",
+                        "transmission": "transmission",
+                        "clamav": "clamav",
+                        "fail2ban": "fail2ban",
+                        "ufw": "ufw",
+                        "nmap": "nmap",
+                        "lynx": "lynx",
+                        "elinks": "elinks",
+                        "irssi": "irssi",
+                        "mutt": "mutt",
+                        "transmission-cli": "transmission-cli",
+                        "samba": "samba",
+                        "nfs-common": "nfs-common",
+                        "cifs-utils": "cifs-utils",
+                        "sshfs": "sshfs",
+                        "zip": "zip",
+                        "unzip": "unzip",
+                        "rar": "rar",
+                        "unrar": "unrar",
+                        "gnome-terminal": "gnome-terminal",
+                        "xfce4-terminal": "xfce4-terminal",
+                        "konsole": "konsole",
+                        "terminator": "terminator",
+                        "ranger": "ranger",
+                        "midnight-commander": "midnight-commander",
+                        "synaptic": "synaptic",
+                        "filezilla": "filezilla",
+                        "putty": "putty",
+                        "xrdp": "xrdp",
+                        "remmina": "remmina",
+                        "pidgin": "pidgin",
+                        "hexchat": "hexchat",
+                        "teamviewer": "teamviewer",
+                        "wine": "wine",
+                        "clamtk": "clamtk",
+                        "xclip": "xclip",
+                        "meld": "meld",
+                        "bleachbit": "bleachbit",
+                        "gufw": "gufw",
+                        "gdebi": "gdebi",
+                        "keepassxc": "keepassxc",
+                        "transmission-gtk": "transmission-gtk",
+                        "qbittorrent": "qbittorrent",
+                        "deluge": "deluge",
+                        "hexedit": "hexedit",
+                        "radare2": "radare2",
+                        "netcat": "netcat",
+                        "john": "john",
+                        "hydra": "hydra",
+                        "nikto": "nikto",
+                        "aircrack-ng": "aircrack-ng",
+                        "ettercap-graphical": "ettercap-graphical",
+                        "tcpdump": "tcpdump",
+                        "openssh-client": "openssh-client",
+                        "rsync": "rsync",
+                        "ffmpeg": "ffmpeg",
+                        "audacity": "audacity",
+                        "darktable": "darktable",
+                        "krita": "krita",}
 
-environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
-template = environment.get_template("template-dockerfile.txt")
+    environment = jinja2.Environment(loader=jinja2.FileSystemLoader("templates/"))
+    template = environment.get_template("template-dockerfile.txt")
 
-OS_image="ubuntu"
-OS_image_version="latest"
-message = "testing message 123"
-path = '/home/ola/Desktop/test_files'
-use_requirements, file_names = use_requirements(path=path)
-content = template.render(OS_image=OS_image,
-                          OS_image_version=OS_image_version,
-                          packages_to_install=pip_packages.values(),
-                          apt_get_packages=apt_get_packages.values(),
-                          use_requirements=use_requirements,
-                          file_names=file_names,
-                          ranges=len(use_requirements))
+    OS_image="ubuntu"
+    OS_image_version="latest"
+    message = "testing message 123"
+    path = '/home/ola/Desktop/test_files'
+    use_req, file_names = use_requirements(path=path)
+    copy_folder_to_dockerfile = add_files.copy_folder_to_dockerfile()
+    content = template.render(OS_image=OS_image,
+                              OS_image_version=OS_image_version,
+                              packages_to_install=pip_packages.values(),
+                              apt_get_packages=apt_get_packages.values(),
+                              use_requirements=use_req,
+                              file_names=file_names,
+                              ranges=len(use_req),
+                             copy_folder_to_dockerfile=copy_folder_to_dockerfile)
 
+    filename = "Dockerfile"
+    with open(os.path.join(add_files.get_working_directory(), filename), "w") as file:
+        file.write(content)
 
-with open("outputs/Dockerfile", "w") as file:
-    file.write(content)
-
-print(content)
+    print(content)
 
 print("Try to run Dockerfile")
 # subprocess.call(["bash", "scripts/dockerfile_runner.sh"])
@@ -144,21 +149,20 @@ center_y = int(screen_height/2 - window_height / 2)
 
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
 
-# textbox for inputting text
-textbox = tk.Text(root, height=5, width=70, pady = 10)
-label = tk.Label(root, text = "Type a message")
+#select folder 
+select_folder_button = tk.Button(root, text = "Select folder", command = lambda:add_files.select_working_directory())
 
 # uploading files
-#upload_file_button = tk.Button(root, text = "Upload file", command = lambda:upload_file())
-file_label = tk.Label(text='Choose a file')
+choose_file_button = tk.Button(root, text = "Choose file", command = lambda:add_files.select_files(root,mode='file'))
+choose_folder_button = tk.Button(root, text = "Choose folder", command = lambda:add_files.select_files(root, mode='dir'))
 
-send_button = tk.Button(root, text = "Send", )
+
+send_button = tk.Button(root, text = "Generate Dockerfile", command=lambda:generate_dockerfile())
 exit_button = tk.Button(root, text = "Exit", command = root.destroy)
 
-label.pack()
-textbox.pack()
-#upload_file_button.pack()
-file_label.pack()
+select_folder_button.pack()
+choose_file_button.pack()
+choose_folder_button.pack()
 send_button.pack()
 exit_button.pack()
 
